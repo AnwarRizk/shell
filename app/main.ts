2e68a16b80565902f60c753386c128f06b834bb5
AnwarRizk <anwar01285593087@gmail.com>
2026-06-25 02:16:02 +0300

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,18 +1,18 @@
 import { createInterface } from 'readline';
 
+function outputCommand(command: string): void {
+  console.log(`${command}: command not found`);
+}
+
 const rl = createInterface({
   input: process.stdin,
   output: process.stdout,
   prompt: '$ ',
 });
 
-// TODO: Uncomment the code below to pass the first stage
 rl.prompt();
-rl.on('line', (command: any) => {
+
+rl.on('line', (command: string) => {
   outputCommand(command);
   rl.prompt();
 });
-
-function outputCommand(command: any) {
-  console.log(`${command}: command not found`);
-}
